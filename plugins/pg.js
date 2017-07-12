544cfe116e8525bba6d0dbe7d934571b91f86e0a
Nathan LaFreniere <quitlahok@gmail.com>
2017-07-12 12:29:36 -0700

    add pg tracer, update trace-agent

---
+++ plugins/pg.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const Shimmer = require('shimmer');
+
+const internals = {};
+
+internals.createWrapper = function (TraceApi) {
+
+  const wrapQuery = function (query) {
+
+    return function traceQuery() {
+
+      const span = TraceApi.createChildSpan({ name: 'pg-query' });
+      const pgQuery = query.apply(this, arguments);
+
+      if (!span) {
+        return pgQuery;
+      }
+
+      span.addLabel('query', pgQuery.text);
+      if (pgQuery.values) {
+        span.addLabel('values', pgQuery.values);
+      }
+
+      TraceApi.wrapEmitter(pgQuery);
+      const done = pgQuery.callback;
+
+      pgQuery.callback = TraceApi.wrap(function (err, res) {
+
+        if (err) {
+          span.addLabel('error', err);
+        }
+
+        if (res) {
+          span.addLabel('row_count', res.rowCount);
+          span.addLabel('oid', res.oid);
+          span.addLabel('rows', res.rows);
+          span.addLabel('fields', res.fields);
+        }
+
+        span.endSpan();
+        if (done) {
+          return done(err, res);
+        }
+      });
+
+      return pgQuery;
+    };
+  };
+
+  return wrapQuery;
+};
+
+module.exports = [{
+  versions: '5.x',
+  patch: function (Pg, TraceApi) {
+
+    Shimmer.wrap(Pg.prototype, 'query', internals.createWrapper(TraceApi));
+  },
+  unpatch: function (Pg) {
+
+    Shimmer.unwrap(Pg.prototype, 'query');
+  }
+}];
